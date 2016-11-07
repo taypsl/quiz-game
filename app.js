f82d97b924c298dc74b064c030ea9697d256cb0e
Taylor Parsell <tayparsell@gmail.com>
2016-11-07 10:08:34 -0800

    fix right/wrong

--- app.js
+++ app.js
@@ -2,66 +2,66 @@ $(document).ready(function() {
 
 // state
 var state = {
-		userChoices: [], 
-		pageCount: 0,	
-		numberCorrect: 0,
+	userChoices: [], 
+	pageCount: 0,	
+	numberCorrect: 0,
 }
 
 var allQuestions = [{
-		question: "What is Albus Dumbledore's full name?", 
-		choices: ["Albus Aberforth Percival Brian Dumbledore", "Albus Percival Wulfric Brian Dumbledore", "Albus Peveral Wulfric Brian Dumbledore", "Albus Percival Wilfred Dumbledore"], 
-		correctAnswer:1
-	}, {
-		question: "What is the name of the ice cream shop in Diagon Alley?",
-		choices: ["Madam Malkins", "Florence Fountaineaus", "Bergen and Borks", "Florean Fortescues"], 
-		correctAnswer:3
-	}, {
-		question: "Where do Harry and Cho have their first date?", 
-		choices: ["The Yule Ball", "The Three Broomsticks", "The Room of Requirement", "Mrs. Puddlemeir's"], 
-		correctAnswer:3
-	}, {
-		question: "When Harry asks Dumbledore what he sees when he looks into the Mirror of Erised, what does Dumbledore tell him he sees?", 
-		choices: ["getting a pair of socks", "his family back with him", "Voldemort defeated", "himself as Minister of Magic"], 
-		correctAnswer:0
-	}, {
-		question: "In Harry Potter and the Goblet of Fire, what is the name of the Muggle family that managed the campsite at the Quidditch World Cup?", 
-		choices: ["Davies", "Williams", "Rogers", "Smith"], 
-		correctAnswer:2
-	}, {
-		question: "What position did Regulus Black play on the Slytherin Quidditch team?", 
-		choices: ["Seeker", "Keeper", "Beater", "Chaser"], 
-		correctAnswer:0
-	}, {
-		question: "Which magical creature carries away Dolores Umbridge at the end of Harry Potter and the Order of the Phoenix?", 
-		choices: ["A Giant", "A herd of Centaurs", "An acromantula", "A werewolf"], 
-		correctAnswer:1
-	}, {
-		question: "Who was the first student petrified by the Basilisk?", 
-		choices: ["Hannah Abbot", "Ernie McMillen", "Penelope Clearwater", "Justin Finch-Fletchley"], 
-		correctAnswer:3
-	}, {
-		question: "Which of the following creatures <u>doesn't</u> Harry encounter in the Forbidden Forest?", 
-		choices: ["Centaurs", "Thestrals", "Red Capps", "Unicorns"], 
-		correctAnswer:2
-	}, {
-		question: "What failed spell does Professor Lockheart use to rid his classroom of Pixies?", 
-		choices: ["Pixie Pesky Problemo", "Pesky Pixie Pestronomy", "Peskinium Pixie", "Pix Pix Peskinium"], 
-		correctAnswer:1
-	}];
+	question: "What is Albus Dumbledore's full name?", 
+	choices: ["Albus Aberforth Percival Brian Dumbledore", "Albus Percival Wulfric Brian Dumbledore", "Albus Peveral Wulfric Brian Dumbledore", "Albus Percival Wilfred Dumbledore"], 
+	correctAnswer:1
+}, {
+	question: "What is the name of the ice cream shop in Diagon Alley?",
+	choices: ["Madam Malkin's", "Florence Fountaineaus", "Borgin and Burkes", "Florean Fortescue's"], 
+	correctAnswer:3
+}, {
+	question: "Where do Harry and Cho have their first date?", 
+	choices: ["The Yule Ball", "The Three Broomsticks", "The Room of Requirement", "Madam Puddifoot's"], 
+	correctAnswer:3
+}, {
+	question: "When Harry asks Dumbledore what he sees in the Mirror of Erised, what does Dumbledore tell him he sees?", 
+	choices: ["getting a pair of socks", "his family back with him", "Voldemort defeated", "himself as Minister of Magic"], 
+	correctAnswer:0
+}, {
+	question: "In 'Harry Potter and the Goblet of Fire', what was the name of the Muggle family that managed the campsite at the Quidditch World Cup?", 
+	choices: ["Davies", "Williams", "Roberts", "Smith"], 
+	correctAnswer:2
+}, {
+	question: "What position did Regulus Black play on the Slytherin Quidditch team?", 
+	choices: ["Seeker", "Keeper", "Beater", "Chaser"], 
+	correctAnswer:0
+}, {
+	question: "Which magical creatures carry away Dolores Umbridge at the end of 'Harry Potter and the Order of the Phoenix?'", 
+	choices: ["Giants", "A herd of Centaurs", "An nest of Acromantula", "A swarm of Dementors"], 
+	correctAnswer:1
+}, {
+	question: "Who was the first student petrified by the Basilisk?", 
+	choices: ["Penelope Clearwater", "Ernie McMillen", "Justin Finch-Fletchley", "Colin Creevey"], 
+	correctAnswer:3
+}, {
+	question: "Which of the following creatures doesn't Harry encounter in the Forbidden Forest?", 
+	choices: ["Centaurs", "Thestrals", "Hinkypunks", "Unicorns"], 
+	correctAnswer:2
+}, {
+	question: "What failed spell does Professor Lockheart use to rid his classroom of Pixies?", 
+	choices: ["Pixiepesky Problemo", "Peskipiksi Pesternomi", "Peskinium Pixie", "Pixpix Peskinium"], 
+	correctAnswer:1
+}];
 
 var results = [{
-		score: 25, 
-		output: "Not to worry, not to worry, there's still time to prepare before December 17th."
-	}, {
-		score: 50, 
-		output: "Not half bad. You'll be a great wizard, once you've been trained up a bit."
-	}, {
-		score: 75, 
-		output: "Jumpin' Gargoyles, that score is not too shabby!"
-	}, {
-		score: 100, 
-		output: "Merlin's pants! With scores like that you could give Hermione Granger a run."
-	}];
+	score: 25, 
+	output: "Not to worry, not to worry, there's still time to prepare before December 17th."
+}, {
+	score: 50, 
+	output: "Not half bad. You'll be a great wizard, once you've been trained up a bit."
+}, {
+	score: 75, 
+	output: "Jumpin' Gargoyles, that score is not too shabby!"
+}, {
+	score: 100, 
+	output: "Merlin's pants! With scores like that you could give Hermione Granger a run."
+}];
 
 // game logic 
 var increasePageCount = function(state) {
@@ -69,25 +69,26 @@ var increasePageCount = function(state) {
 };
 
 var addUserChoice = function(state, choice) {
-  state.userChoices[state.pageCount] = choice;
+	state.userChoices[state.pageCount] = choice;
 };
 
-var quizScore = function(state, correctAnswer) {
-	var finalScore = (numberCorrect / 10) * 100;
+var quizScore = function(state) {
+	var finalScore = (state.numberCorrect / 10) * 100;
+	return finalScore;
 };
-	
-var resultsText = function(quizScore) {
-	if (quizScore <= 25) {
-		results[0].output //add listener later? append to page later? 
+
+var resultsText = function(state) {
+	if (quizScore(state) <= 25) {
+		return results[0].output //add listener later? append to page later? 
 	}
-	else if (quizScore > 25 && finalScore <=50) {
-		results[1].output;
+	else if (quizScore(state) > 25 && quizScore(state) <=50) {
+		return results[1].output;
 	}
-	else if (quizScore > 50 && finalScore <=75) {
-		results[2].output;
+	else if (quizScore(state) > 50 && quizScore(state) <=75) {
+		return results[2].output;
 	}
 	else {
-		results[3].output;
+		return results[3].output;
 	}
 };
 
@@ -96,38 +97,42 @@ var displayNextQuestion = function(state, allQuestions) {
 	$('.question').text(allQuestions[state.pageCount].question); //display question
 
 	var choices = allQuestions[state.pageCount].choices;		 //display answer choices
-    for (var i=0; i<choices.length; i++) {
-      $('.button-label#' + i).text(choices[i]);
-    }
+	for (var i=0; i<choices.length; i++) {
+		$('.answer-choice#' + i).find('span').text(choices[i]);
+	}
+	$('.page-number').text('Page ' + (state.pageCount + 1) + '/10')
+
 };
 
 var checkAnswer = function(state) {
+	$('.answer-choice#' + allQuestions[state.pageCount].correctAnswer).addClass('right-answer');
 	if (state.userChoices[state.pageCount] == allQuestions[state.pageCount].correctAnswer) { 
-		$('.button-label#' + allQuestions[state.pageCount].correctAnswer).closest('button').css('border', '3px solid green');
 		state.numberCorrect++; 
-		console.log('right');
 	} 
 	else {
-		$(this).closest('button').css('border', '3px solid red');	//('this') is calling .button-label?	
-		$('.button-label#' + allQuestions[state.pageCount].correctAnswer).closest('button').css('border', '3px solid green');
-  		console.log('wrong');
-  }
-  $('#next').toggleClass('hidden');
+		$('.answer-choice#' + state.userChoices[state.pageCount]).addClass('wrong-answer');		
+	}
+	$('#next').toggleClass('hidden');
 };
+
 
 // event handlers
 $('.start-quiz').click(function(e) {
 	displayNextQuestion(state, allQuestions);
 	state.pageCount = 0;
+	state.userChoices = [];
+	state.numberCorrect = 0;
 	$('.quiz-page').removeClass('hidden');
 	$('.answer-choice').removeClass('hidden');
 	$('.intro').addClass('hidden');
+	$('.start-quiz').addClass('hidden');
+
 });
 
-$('.answer-choice').on('click', '.button-label', function(e) {
+$('.answer-choice').on('click', function(e) {
 	var choice = $(this).attr('id');
-  	addUserChoice(state, choice);
-  	checkAnswer(state);
+	addUserChoice(state, choice);
+	checkAnswer(state);
 
 	$('.answer-choice[type=submit]').prop('disabled', true); //disable button
 
@@ -135,24 +140,26 @@ $('.answer-choice').on('click', '.button-label', function(e) {
 
 $('#next').click(function(e) {
 	e.preventDefault();
-	increasePageCount(state);
-	displayNextQuestion(state, allQuestions);
-	$('.answer-choice[type=submit').prop('disabled', false); //re-enable button
-	$('#next').toggleClass('hidden');
-	$('.page-number').text('Page ' + state.pageCount + '/10')
-	if (state.pageCount === 10) {
-		$('.quiz-page>.answer-choice.hidden').addClass('hidden');
-		$('.results').removeClass('hidden');
-		$('h1').text(quizScore);
-		$('.score').text(resultsText);
-		$('.page-number').addClass('hidden');
-	}
-	console.log(state.pageCount);
 	console.log(state.numberCorrect);
+
+	if (state.pageCount < (allQuestions.length-1)) {
+		increasePageCount(state);
+		displayNextQuestion(state, allQuestions);
+		$('.answer-choice[type=submit').prop('disabled', false); //re-enable button
+		$('#next').toggleClass('hidden');
+		$('.answer-choice').removeClass('wrong-answer');		
+		$('.answer-choice').removeClass('right-answer');		
+	}else{
+		console.log(state.finalScore);
+		$('h1').text(quizScore(state));
+		$('.score').text(resultsText(state));
+
+		$('.quiz-page').addClass('hidden');
+		$('.results').removeClass('hidden');
+		$('.start-quiz').removeClass('hidden');
+		$('.page-number').addClass('hidden');
+	}    
 });
-
-
-
 
 }); 
 
